7d42fec5670d0c66d85febc5316402dce01f1005
AdminFloreville <adminloc@floreville.ru>
2025-05-19 09:12:17 +0300

    исправил веб сокеты

--- frontend/src/hooks/useCameraSocket.ts
+++ frontend/src/hooks/useCameraSocket.ts
@@ -4,7 +4,7 @@ import { cameraStore } from '../stores/cameraStore';
 
 export const useCameraSocket = () => {
   useEffect(() => {
-    const socket = io('http://localhost:5444'); // или твой адрес
+    const socket = io(import.meta.env.VITE_API_BASE_URL); // или твой адрес
 
     socket.on('cameraStatus', ({ id, isOnline }) => {
       cameraStore.updateStatus(id, isOnline);
--- frontend/src/hooks/usePrinterSocket.ts
+++ frontend/src/hooks/usePrinterSocket.ts
@@ -4,7 +4,7 @@ import { printerStore } from '../stores/printerStore';
 
 export const usePrinterSocket = () => {
   useEffect(() => {
-    const socket = io('http://localhost:3000'); // или твой адрес
+    const socket = io(import.meta.env.VITE_API_BASE_URL);
 
     socket.on('printer-status', ({ id, isOnline }) => {
       printerStore.updateStatus(id, isOnline);
